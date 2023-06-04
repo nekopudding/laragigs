784989a2146228253b2b64477450aacdaa099423
nekopudding <deany.y.ca@gmail.com>
2023-06-04 12:48:38 -0700

    database setup - migration, seeder, factory, listing model

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -2,30 +2,10 @@
 
 namespace App\Models;
 
-class Listing {
-  public static function all() {
-    return [
-      [
-          'id' => 1,
-          'title' => 'Listing 1',
-          'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl eget ultricies aliquam, quam libero ultricies nunc, quis aliquet nisl nunc quis nisl. Sed euismod, nisl eget ultricies aliquam, quam libero ultricies nunc, quis aliquet nisl nunc quis nisl.',
-      ],
-      [
-          'id' => 2,
-          'title' => 'Listing 2',
-          'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl eget ultricies aliquam, quam libero ultricies nunc, quis aliquet nisl nunc quis nisl. Sed euismod, nisl eget ultricies aliquam, quam libero ultricies nunc, quis aliquet nisl nunc quis nisl.',
-      ]
-    ]; 
-  }
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-  public static function find($id) {
-    $listings = self::all();
-    foreach ($listings as $listing) {
-      if ($listing['id'] == $id) { // == for loose comparison
-        return $listing;
-      }
-    }
-  }
+class Listing extends Model
+{
+    use HasFactory;
 }
-
-?>
